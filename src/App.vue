25e3c5a3b8b8e51d171ab5384b45812d983bbb7a
Shubham <shubhamp@auberginesolutions.com>
2020-04-01 09:50:21 +0530

    Implement Mixins

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,24 @@
 <template>
     <div>
-        <form-helper>
-            <div slot="form-header">
-                <h3>This is the title of a form</h3>
-                <p>This is some info about the form</p>
-            </div>
-            <div slot="form-fields">
-                <input type="text" placeholder="name" required />
-                <input type="password" placeholder="password" required />
-            </div>
-            <div slot="form-controls">
-                <button v-on:click="handleSubmit">Submit</button>
-            </div>
-        </form-helper>
+        <show-blogs></show-blogs>
+        <list-blogs></list-blogs>
     </div>
 </template>
 
 <script>
 // Imports
-import formHelper from './components/formHelper.vue'
+import showBlogs from './components/showBlogs.vue';
+import listBlogs from './components/listBlogs.vue';
 export default {
     components: {
-        'form-helper': formHelper
+        'show-blogs': showBlogs,
+        'list-blogs': listBlogs
     },
     data () {
         return {
         }
     },
     methods: {
-        handleSubmit: function(){
-            alert('thanks for submitting');
-        }
     }
 }
 </script>
